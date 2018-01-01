f220ff0c826ebff8b262c8095654153199b24d31
Sanchit-Aggarwal <sancool1995@gmail.com>
2018-01-01 15:17:47 +0530

    Basic Template

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+var count=0;
+var pointer;
 
 class App extends Component {
+
+
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div id='head'>
+             <center><h1>Welcome,Jai Mata Di ,Happy Coding :)</h1></center>
+        </div>
+        <br />
+        <div className="main row ">
+        <div className='col'>
+             <div className="jumbotron">
+
+                <div className='row'>
+                <label id='addSpace1'><strong><h4>Enter task name</h4></strong></label>
+                <input id='feild' type='text' className='form-control col-sm-4'></input>
+                <button id='addSpace' className='btn  btn-primary col-sm-3' onClick={this.addToList}>Add it to list</button>
+                </div>
+
+
+             </div>
+        </div>
+        <div className='col'>
+        <div className="jumbotron full">
+        <div id="myList">
+
+
+        </div>
+
+        </div>
+        </div>
+        </div>
+
+
       </div>
     );
   }
